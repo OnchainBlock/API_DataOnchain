561122106350695ef1351cac6a3f95ccece97c84
DoVanThang-AI <79442911+DoVanThang-AI@users.noreply.github.com>
2023-10-17 17:22:22 +0700

    config table L2 tx overview

--- Layer2_data/L2_overviews.py
+++ Layer2_data/L2_overviews.py
@@ -3,6 +3,7 @@ sys.path.append(r'/Users/dev/Thang_DataEngineer/API_DataOnchain')
 from imports import *
 query_l2 = os.environ['query_l2']
 server = os.environ['my_server']
+query_tvl = os.environ['query_ETH_bridge']
 
 L2_eth = pd.read_sql(query_l2,server)
 condition_time = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -16,6 +17,12 @@ weekly_df =weekly_df.groupby(['chain']).resample('W', on='dt')[['eth_amount', 'u
                                            'volume_bridge', 'fee_volume', 'tx', 'chain']].sum(numeric_only=True).sort_values(by=['dt'],ascending=True).reset_index()
 
 weekly_df =weekly_df[weekly_df['dt']<= condition_time]
+
+TVL_df = pd.read_sql(query_tvl,server)
+
+# config format time
+TVL_df['time'] = TVL_df['time'].apply(
+    lambda x: pd.to_datetime(x).floor('T'))
 #treemap
 def treemap(condition:str):
     l2_condition = ['bridger','amount','fee','tx']
@@ -46,9 +53,10 @@ def create_table_overview():
     volume_change =[vl - ql for vl,ql in zip(table['eth_amount'],Qk_table['eth_amount'])]
     fee_change = [vl - ql for vl,ql in zip(table['fee_tx'],Qk_table['fee_tx'])]
     tx_change = [vl - ql for vl,ql in zip(table['tx'],Qk_table['tx'])]
-
+    tvl =TVL_df[TVL_df['time']==TVL_df['time'].max()].sort_values(by=['bridge'],ascending=False)[['value']]
     data = pd.DataFrame({
         'chain':[i for i in table['chain']],
+        'tvl':[i for i in tvl['value']],
         'vl_change':volume_change,
         'fee_change':fee_change,
         'tx_change':tx_change,
@@ -56,23 +64,9 @@ def create_table_overview():
         'per_fee':[round((qk/vl)*100,2) for qk,vl in zip(fee_change,table['fee_tx'])],
         'per_tx':[round((qk/vl)*100,2) for qk,vl in zip(tx_change,table['tx'])]
     })
-    return data.sort_values(by=['vl_change'],ascending=False).to_dict(orient="records")
+    return data.sort_values(by=['tvl'],ascending=False).to_dict(orient="records")
 
-#create statics
-# def create_statics_L2(l2:str):
-#     l2_condition = ['starknet', 'arbitrum', 'polygon', 'optimsn', 'zk_era', 'base',
-#        'mantle', 'linear', 'manta','scroll']
-    
-#     if l2 not in l2_condition:
-#         return f'layer2: {l2} is not found, plase choice another["starknet", "arbitrum", "polygon", "optimsn", "zk_era", "base","mantle", "linear", "manta","scroll"]'
-#     else:
-#         data = L2_eth[L2_eth['chain']==l2]
-#     return pd.DataFrame({
-#         'Total_User':[sum(data['unique_users'])],
-#         'Total_ETH':[sum(data['eth_amount'])],
-#         'Total_Tx':[sum(data['tx'])],
-#         'Total_Fee':[sum(data['fee_tx'])]
-#     }).to_dict(orient="records")
+
 def create_statics_L2(condition:str):
     l2_condition = ['bridger','amount','fee','tx']
     data = L2_eth[L2_eth['dt']== L2_eth['dt'].max()]
