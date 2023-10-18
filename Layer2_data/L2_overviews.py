2ea670615248512f6fd292b3d06a64fa447eb2c4
DoVanThang-AI <79442911+DoVanThang-AI@users.noreply.github.com>
2023-10-18 17:24:24 +0700

    rename in Frontend

--- Layer2_data/L2_overviews.py
+++ Layer2_data/L2_overviews.py
@@ -27,28 +27,30 @@ TVL_df['time'] = TVL_df['time'].apply(
 def treemap(condition:str):
     l2_condition = ['bridger','amount','fee','tx']
     data = L2_eth.groupby(['chain']).agg({'eth_amount':'sum','unique_users':'sum','fee_tx':'sum','tx':'sum'}).sort_values(by=['tx'],ascending=False).reset_index()
+    data['chain'] = data['chain'].map(lambda x : x.title())
     if condition not in l2_condition:
         return f'layer2: {condition} is not found, plase choice another["bridger","amount","fee","tx"]'
     elif condition =='bridger':
         size =[500,250,150,100,60,40,32,30,26,10]
         data['size'] = [i for i in size[:len(data)]]
-        return data[['chain','unique_users','size']].to_dict(orient="records")
+        return data[['chain','unique_users','size']].rename(columns={'chain':'label','unique_users':'value'}).to_dict(orient="records")
     elif condition =='amount':
         size =[500,250,150,100,60,40,32,30,26,10]
         data['size'] = [i for i in size[:len(data)]]
-        return data[['chain','eth_amount','size']].to_dict(orient="records")
+        return data[['chain','eth_amount','size']].rename(columns={'chain':'label','eth_amount':'value'}).to_dict(orient="records")
     elif condition =='fee':
         size =[500,250,150,100,60,40,32,30,26,10]
         data['size'] = [i for i in size[:len(data)]]
-        return data[['chain','fee_tx','size']].to_dict(orient="records")
+        return data[['chain','fee_tx','size']].rename(columns={'chain':'label','fee_tx':'value'}).to_dict(orient="records")
     elif condition =='tx':
         size =[500,250,150,100,60,40,32,30,26,10]
         data['size'] = [i for i in size[:len(data)]]
-        return data[['chain','tx','size']].to_dict(orient="records")
+        return data[['chain','tx','size']].rename(columns={'chain':'label','tx':'value'}).to_dict(orient="records")
 
 # overview table
 def create_table_overview():
     table = L2_eth[L2_eth['dt'] ==  L2_eth['dt'].max()].sort_values(by=['chain'],ascending=False)[['eth_amount','fee_tx','tx','chain']]
+    table['chain']= table['chain'].map(lambda x : x.title())
     Qk_table =L2_eth[L2_eth['dt'] ==  L2_eth['dt'].max() - datetime.timedelta(days=1)].sort_values(by=['chain'],ascending=False)[['eth_amount','fee_tx','tx','chain']]
     volume_change =[vl - ql for vl,ql in zip(table['eth_amount'],Qk_table['eth_amount'])]
     fee_change = [vl - ql for vl,ql in zip(table['fee_tx'],Qk_table['fee_tx'])]
@@ -70,16 +72,17 @@ def create_table_overview():
 def create_statics_L2(condition:str):
     l2_condition = ['bridger','amount','fee','tx']
     data = L2_eth[L2_eth['dt']== L2_eth['dt'].max()]
+    data['chain'] = data['chain'].map(lambda x : x.title())
     if condition not in l2_condition:
         return f'layer2: {condition} is not found, plase choice another["bridger","amount","fee","tx"]'
     elif condition =="bridger":
-        return data[['chain','unique_users']].to_dict(orient='records')
+        return data[['chain','unique_users']].rename(columns={'chain':'label','unique_users':'value'}).to_dict(orient='records')
     elif condition =="amount":
-        return data[['chain','eth_amount']].to_dict(orient='records')
+        return data[['chain','eth_amount']].rename(columns={'chain':'label','eth_amount':'value'}).to_dict(orient='records')
     elif condition =="fee":
-        return data[['chain','fee_tx']].to_dict(orient='records')
+        return data[['chain','fee_tx']].rename(columns={'chain':'label','fee_tx':'value'}).to_dict(orient='records')
     elif condition =="tx":
-        return data[['chain','tx']].to_dict(orient='records')
+        return data[['chain','tx']].rename(columns={'chain':'label','tx':'value'}).to_dict(orient='records')
 
 # create dataFrame for 
 class Func_Layer2():
@@ -92,21 +95,22 @@ class Func_Layer2():
         if l2 not in l2_condition:
             return f'layer2: {l2} is not found, plase choice another["starknet", "arbitrum", "polygon", "optimsn", "zk_era", "base","mantle", "linear", "manta","scroll"]'
         data = L2_eth[L2_eth['chain']==l2]
+        data['chain']= data['chain'].map(lambda x: x.title())
         data['time_select'] = pd.to_datetime(data['dt']).dt.date
         data['time_select'] = pd.to_datetime(data['time_select'])
         data = data[data['time_select'].between(start,end)]
         if col_condition == 'unique_users':
             cols=['dt','chain','unique_users']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','unique_users':'value'}).to_dict(orient="records")
         elif col_condition=='eth_amount':
             cols =['dt','chain','eth_amount']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','eth_amount':'value'}).to_dict(orient="records")
         elif col_condition=='tx':
             cols =['dt','chain','tx']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','tx':'value'}).to_dict(orient="records")
         elif col_condition=='fee_tx':
             cols =['dt','chain','fee_tx']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','fee_tx':'value'}).to_dict(orient="records")
     def Weekly(l2:str,start:str,end:str,col_condition:str):
         l2_condition = ['starknet', 'arbitrum', 'polygon', 'optimsn', 'zk_era', 'base',
        'mantle', 'linear', 'manta','scroll']
@@ -114,18 +118,19 @@ class Func_Layer2():
         if l2 not in l2_condition:
             return f'layer2: {l2} is not found, plase choice another["starknet", "arbitrum", "polygon", "optimsn", "zk_era", "base","mantle", "linear", "manta","scroll"]'
         data = weekly_df[weekly_df['chain']==l2]
+        data['chain']= data['chain'].map(lambda x: x.title())
         data['time_select'] = pd.to_datetime(data['dt']).dt.date
         data['time_select'] = pd.to_datetime(data['time_select'])
         data = data[data['time_select'].between(start,end)]
         if col_condition == 'unique_users':
             cols=['dt','chain','unique_users']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','unique_users':'value'}).to_dict(orient="records")
         elif col_condition=='eth_amount':
             cols =['dt','chain','eth_amount']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','eth_amount':'value'}).to_dict(orient="records")
         elif col_condition=='tx':
             cols =['dt','chain','tx']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','tx':'value'}).to_dict(orient="records")
         elif col_condition=='fee_tx':
             cols =['dt','chain','fee_tx']
-            return data[cols].to_dict(orient="records")
+            return data[cols].rename(columns={'dt':'timestamp','chain':'label','fee_tx':'value'}).to_dict(orient="records")
